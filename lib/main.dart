d93aec0ac6a5752b9262000f559c47ea16d8180a
Alex-Yu21 <yualex2112@gmail.com>
2025-04-20 18:30:44 +0400

    refactor: move colors to ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tetris/board.dart';
+import 'package:tetris/presentation/screens/board.dart';
 
 void main() {
   runApp(TetrisGame());
@@ -10,6 +10,30 @@ class TetrisGame extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: BoardScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFFFF8E8),
+        primaryColor: const Color(0xFF744E2E),
+        cardTheme: CardTheme(
+          color: const Color(0xFFFFF5DD),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: const BorderSide(color: Color(0xFF744E2E), width: 3),
+          ),
+          elevation: 4,
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF744E2E),
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Color(0xFF744E2E), size: 60),
+      ),
+      home: BoardScreen(),
+    );
   }
 }
